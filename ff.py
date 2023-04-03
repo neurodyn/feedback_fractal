6ef493a1877c6893c2bc0ac628c53fa0ad59951f
Alexander Woodward <alexander.woodward@riken.jp>
2023-04-03 16:07:21 +0900

    Refactored.

--- ff.py
+++ ff.py
@@ -44,51 +44,47 @@ def run():
         c3 = math.cos(0.015 * counter)
         c4 = math.cos(0.0003 * counter)
 
-        m_1 = cv2.getRotationMatrix2D(
-            (WIDTH / 2 + c1 * 20, HEIGHT / 2 + c2 * 20), angle * c1, 0.8 * c1
-        )
-        m_1.resize((3, 3))
-        m_1[2, 2] = 1
-        m_2 = np.array(
-            [[1.0, 0, WIDTH * 0.3 * c1], [0, 1.0, HEIGHT * 0.2 * c4], [0, 0, 1]]
-        )
-        m_3 = np.dot(m_2, m_1)
+        m1 = cv2.getRotationMatrix2D(
+            (WIDTH / 2 + c1 * 20, HEIGHT / 2 + c2 * 20), angle * c1, 0.8 * c1)
+        m1.resize((3, 3))
+        m1[2, 2] = 1
+        m2 = np.array([[1.0, 0, WIDTH * 0.3 * c1], [0, 1.0, HEIGHT * 0.2 * c4],
+                       [0, 0, 1]])
+        m3 = np.dot(m2, m1)
         # assume a square template image
         img_r1 = cv2.warpPerspective(
             img_current,
-            m_3,
+            m3,
             (WIDTH, HEIGHT),
         )
 
-        m_1 = cv2.getRotationMatrix2D(
-            (WIDTH / 2 + c3 * 20, HEIGHT / 2 + c2 * 20), 125 + angle * c2, 0.6 * c2
-        )
-        m_1.resize((3, 3))
-        m_1[2, 2] = 1
-        m_2 = np.array(
-            [[1.0, 0, -WIDTH * 0.4 * c3], [0, 1.0, -HEIGHT * 0.1 * c4], [0, 0, 1]]
-        )
-        m_3 = np.dot(m_2, m_1)
+        m1 = cv2.getRotationMatrix2D(
+            (WIDTH / 2 + c3 * 20, HEIGHT / 2 + c2 * 20), 125 + angle * c2,
+            0.6 * c2)
+        m1.resize((3, 3))
+        m1[2, 2] = 1
+        m2 = np.array([[1.0, 0, -WIDTH * 0.4 * c3],
+                       [0, 1.0, -HEIGHT * 0.1 * c4], [0, 0, 1]])
+        m3 = np.dot(m2, m1)
         # assume a square template image
         img_r2 = cv2.warpPerspective(
             img_current,
-            m_3,
+            m3,
             (WIDTH, HEIGHT),
         )
 
-        m_1 = cv2.getRotationMatrix2D(
-            (WIDTH / 2 + c2 * 20, HEIGHT / 2 + c4 * 20), 25 + angle * c4, 0.3 * c3
-        )
-        m_1.resize((3, 3))
-        m_1[2, 2] = 1
-        m_2 = np.array(
-            [[1.0, 0, -WIDTH * 0.5 * c3], [0, 1.0, -HEIGHT * 0.4 * c4], [0, 0, 1]]
-        )
-        m_3 = np.dot(m_2, m_1)
+        m1 = cv2.getRotationMatrix2D(
+            (WIDTH / 2 + c2 * 20, HEIGHT / 2 + c4 * 20), 25 + angle * c4,
+            0.3 * c3)
+        m1.resize((3, 3))
+        m1[2, 2] = 1
+        m2 = np.array([[1.0, 0, -WIDTH * 0.5 * c3],
+                       [0, 1.0, -HEIGHT * 0.4 * c4], [0, 0, 1]])
+        m3 = np.dot(m2, m1)
         # assume a square template image
         img_r3 = cv2.warpPerspective(
             img_current,
-            m_3,
+            m3,
             (WIDTH, HEIGHT),
         )
 
@@ -97,7 +93,8 @@ def run():
         img_current = cv2.addWeighted(img_r1, 1.0, img_r2, 1.0, 0)
         img_current = cv2.addWeighted(img_current, 1.0, img_r3, 1.0, 0)
         # zoom it
-        m4 = cv2.getRotationMatrix2D((WIDTH / 2, HEIGHT / 2), 0, 1.2 + 0.2 * c3)
+        m4 = cv2.getRotationMatrix2D((WIDTH / 2, HEIGHT / 2), 0,
+                                     1.2 + 0.2 * c3)
         img_current = cv2.warpAffine(
             img_current,
             m4,
@@ -106,8 +103,8 @@ def run():
         cv2.normalize(img_current, img_current, 0, 255, cv2.NORM_MINMAX)
         # apply a color map
         img_current = cv2.addWeighted(
-            img_current, 0.9, cv2.applyColorMap(img_current, cv2.COLORMAP_HSV), 0.1, 0
-        )
+            img_current, 0.9, cv2.applyColorMap(img_current, cv2.COLORMAP_HSV),
+            0.1, 0)
         # compose it with previous iteration
         img_current = cv2.addWeighted(img_current, 0.9, img_previous, 0.1, 0)
         img_previous = img_current.copy()
